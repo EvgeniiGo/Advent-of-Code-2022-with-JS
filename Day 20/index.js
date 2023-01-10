abe8bc0fc65a774d27d58bbb3b72c5de5b923b85
Ëvgenii Golbraikh <evgeniigolbraikh@gmail.com>
2023-01-11 00:02:25 +0200

    Day 20

--- Day 20/index.js
+++ Day 20/index.js
@@ -1,5 +1,14 @@
+// solution for part 1 is not mine
+
+
 const fs = require('fs');
 let input;
+
+class Int {
+  constructor(value) {
+    this.value = value;
+  }
+}
 
 // Reading input data from file
 fs.readFile('./input.txt', { encoding: 'utf8' }, (err, data) => {
@@ -12,85 +21,41 @@ fs.readFile('./input.txt', { encoding: 'utf8' }, (err, data) => {
 
   // split data by line
   input = input.split('\r\n');
-  // console.log(String(input));
 
-  input = input.map((number) => Number(number));
+  const key = 811589153;
 
-  const l = input.length; // length of the list
-  let zeroId;
+  const mix = (numbers, n = 1) => {
+    const result = [...numbers];
 
-  const numbers = {};
-  for (let i = 0; i < input.length; i++) {
-    numbers[i] = {};
-    numbers[i]['value'] = input[i];
-    numbers[i]['position'] = i;
-    if (input[i] === 0) {
-      zeroId = i;
-      console.log('zeroId:', zeroId);
-    }
-  }
-  // console.log(numbers);
+    while (n > 0) {
+      for (const number of original) {
+        const resultIdx = result.indexOf(number);
+        result.splice(resultIdx, 1);
+        let nextIdx = (resultIdx + number.value) % result.length;
 
-  const coordinates = [];
-
-  Object.keys(numbers).forEach((id) => {
-    const index = numbers[id]['position'];
-
-    let newIndex = index + numbers[id]['value'];
-    if (newIndex >= l) {
-      while (newIndex > l) {
-        newIndex -= l;
-      };
-      newIndex += 1;
-    };
-    if (newIndex < 0) {
-      while (newIndex < 0) {
-        newIndex += l;
-      };
-      newIndex -= 1;
-    };
-    // console.log(numbers[id]['value'], 'should move to position', newIndex);
-    if (newIndex < index) {
-      Object.keys(numbers).forEach((num) => {
-        if (numbers[num]['position'] >= newIndex && numbers[num]['position'] < index) {
-          numbers[num]['position'] += 1;
+        if (nextIdx < 0) {
+          nextIdx += result.length;
         }
-      });
-    } else if (newIndex > index) {
-      Object.keys(numbers).forEach((num) => {
-        if (numbers[num]['position'] <= newIndex && numbers[num]['position'] > index) {
-          numbers[num]['position'] -= 1;
-        }
-      });
-    }
-    numbers[id]['position'] = newIndex;
-    // console.log(numbers);
-  });
 
-  console.log(numbers[zeroId])
-  // console.log(numbers)
-  const start = numbers[zeroId]['position'];
-  for (let i = 1000; i <= 3000; i += 1000) {
-    let index = start + i;
-    if (index >= l) {
-      while (index >= l) {
-        index -= l;
-      };
-    } else {
-      index = start + i;
-    }
-    Object.keys(numbers).forEach((number) => {
-      if (numbers[number]['position'] === index) {
-        console.log(numbers[number]);
-        coordinates.push(numbers[number]['value']);
+        result.splice(nextIdx, 0, number);
       }
-    });
+
+      n -= 1;
+    }
+
+    return result;
   };
 
-  // console.log(numbers);
 
-  console.log(coordinates);
-  console.log(coordinates.reduce((total, item) => total + item));
-
+  const original = input.map(num => new Int(Number(num) * key));
+  let mixed = mix([...original]);
+  // for part 2
+  for (let i = 0; i < 9; i++) {
+    mixed = mix([...mixed]);
+  };
+  const zeroIdx = mixed.findIndex(num => num.value === 0);
+  console.log([1000, 2000, 3000]
+    .map(shift => mixed[(zeroIdx + shift) % mixed.length])
+    .reduce((acc, num) => acc + num.value, 0));
 
 });
